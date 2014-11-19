57bbb2a6cf982d922123d30b36c389e0fc8071c4
unknown <panduranga@ULTP_147.synerzipune.local>
2014-11-19 20:58:10 +0530

    Added

--- cookbooks/jenkins/files/default/installGems.sh
+++ cookbooks/jenkins/files/default/installGems.sh
@@ -6,3 +6,13 @@ gem install selenium-webdriver
 sudo -Hiu jenkins
 cd /var/lib/jenkins/jobs
 unzip /var/chef/primefactors.zip
+chown -R jenkins:jenkins primefactors
+cd /var/lib/jenkins/plugins
+unzip /var/chef/jenkins_plugins.zip
+chown -R jenkins:jenkins *
+cd /var/lib/jenkins
+cp /var/chef/credentials.xml .
+cp /var/chef/org.jenkinsci.plugins.xvfb.XvfbBuildWrapper.xml .
+chown jenkins:jenkins credentials.xml
+chown jenkins:jenkins org.jenkinsci.plugins.xvfb.XvfbBuildWrapper.xml
+sudo service jenkins restart
